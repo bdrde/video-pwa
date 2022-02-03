f25a096278d7eb7fb00e5a4344573f9bd77ab077
Michael Rasenberger <michael.rasenberger@bdr.de>
2022-02-03 15:50:26 +0100

    a somewhat inital version

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+console.log('I am running!');
+/*
+window.onload = () => {
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
+  }
+}
+*/
