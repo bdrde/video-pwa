3287700282e50a6d4055be20c6937fce3cabaef1
Michael Rasenberger <michael.rasenberger@bdr.de>
2022-02-04 10:12:54 +0100

    use app scoped links (for now)

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ console.log('I am running!');
 const outlet = document.querySelector('main');
 const router = new Router(outlet);
 router.setRoutes([
-  {path: '/',     component: 'x-login-view'},
-  {path: '/info',  component: 'x-info-view'},
-  {path: '/video', component: 'x-video-view'},
-  {path: '/prescription', component: 'x-prescription-view'}
+  {path: '/video-pwa/',     component: 'x-login-view'},
+  {path: '/video-pwa/info',  component: 'x-info-view'},
+  {path: '/video-pwa/video', component: 'x-video-view'},
+  {path: '/video-pwa/prescription', component: 'x-prescription-view'}
   //{path: '(.*)', component: 'x-login-view'},
 ]);
