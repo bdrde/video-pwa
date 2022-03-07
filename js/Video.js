b024592d85997314f74912f56b6e7ad0850e4227
Michael Rasenberger <michael.rasenberger@bdr.de>
2022-03-07 13:07:56 +0100

    minor styling

--- js/Video.js
+++ js/Video.js
@@ -30,7 +30,7 @@ class Video extends HTMLElement {
                 </div>
                 <h2>Es befindet sich ein Patient im Warteraum</h2>
                 <button class="button light" @click=${_ => this.startVideo()} style="height:40px;">
-                    <img class="button-logo" src="./images/camera-filled.svg" />
+                    <!-- <img class="button-logo" src="./images/camera-filled.svg"/> -->
                     <div class="button-text">Video starten</div>
                 </button>
             </div>
